99fc959d2ae434307c3d63a86c223031c89f3592
gmamadeesh <146164146+gmamadeesh@users.noreply.github.com>
2023-09-27 12:46:00 +0530

    Add files via upload

---
+++ challenge 2.2.py
@@ -0,0 +1,21 @@
+def averageRuns(runs, matches, notout):
+ 
+    out = matches - notout;
+ 
+    if (out == 0):
+        return -1;
+ 
+    avg = runs // out;
+ 
+    return avg;
+ 
+runs = 10000;
+matches = 260;
+notout =30 ;
+ 
+avg = averageRuns(runs, matches, notout);
+ 
+if (avg == -1):
+    print("NA");
+else:
+    print(avg);
